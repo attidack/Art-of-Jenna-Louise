ad863f50527b9079e6d02af8ec5d5750c9505208
attidack <attidack@gmail.com>
2022-08-11 16:55:44 -0600

    start of admin work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
 
+
 const Home = () => {
   return (
     <div className="container">
---
+++ client/src/pages/admin.js
@@ -0,0 +1,6 @@
+import auth from "../utils/auth"
+if (token){
+
+}else {
+    return home
+}
