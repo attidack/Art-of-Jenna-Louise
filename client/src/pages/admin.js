01983992d5a90d8776626db59dbc014f0e815dd2
attidack <attidack@gmail.com>
2022-08-15 20:49:36 -0600

    class save

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 
 const Admin = () => {
-    const { loading, data } = useQuery(QUERY_ALL_USERS);
+    // const { loading, data } = useQuery(QUERY_ALL_USERS);
     return (
       <div className="container">
         admin page
